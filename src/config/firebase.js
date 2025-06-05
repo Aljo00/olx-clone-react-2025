7113ccfb02b8d717dcd82ec93fcfa5d85755f207
Aljo00 <aljojoju950@gmail.com>
2025-06-05 06:56:00 +0530

    update firebase configuration with correct project details

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,15 +3,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAON0zIKHaIaA5vvlYfzBunpCvkLK9v__Y",
-  authDomain: "olx-clone-react-fdd51.firebaseapp.com",
-  projectId: "olx-clone-react-fdd51",
-  storageBucket: "olx-clone-react-fdd51.appspot.com",
-  messagingSenderId: "849105536621",
-  appId: "1:849105536621:web:09e9efac311794805de164"
+  apiKey: "AIzaSyCkL8ZMdx_BGl2PiQPFTCY0JE3iIsGPoyA",
+  authDomain: "olx-react-clone-c8bd7.firebaseapp.com",
+  projectId: "olx-react-clone-c8bd7",
+  storageBucket: "olx-react-clone-c8bd7.appspot.com",
+  messagingSenderId: "32127549495",
+  appId: "1:32127549495:web:198c3e0a61f3919d39bfb2",
 };
-
-
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
